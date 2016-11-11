d21e6a466bde19c88608deb14f0f3aa82bb334aa
Julie Goldberg <Julie@empowerengine.com>
2016-11-11 12:00:44 -0800

    fixed current file path

--- scrape_county_election_results_2016_senate.R
+++ scrape_county_election_results_2016_senate.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 # Load Data ----
 
-wd <- ''
+wd <- '.'
 #load county-fips-state file
 state_county_fips <- read.csv(
   file.path(wd, 'state_county_fips.csv'), stringsAsFactors=FALSE)
