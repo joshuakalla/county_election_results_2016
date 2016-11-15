2433aa8bd9d38a46e70c8ac01789e6fa42b00aae
Julie Goldberg <Julie@empowerengine.com>
2016-11-14 16:50:17 -0800

    convert both csvs, not just 1

--- convert_to_one_row_per_county.py
+++ convert_to_one_row_per_county.py
@@ -1,11 +1,5 @@
 import csv
 
-infile_path = "./county_election_results_2016.csv"
-outfile_path = "./county_election_results_2016_row_per_county.csv"
-
-fields_to_match = ["abbr_state","county","fips"]
-candidate_names = ["Donald Trump", "Hillary Clinton", "Gary Johnson", "Jill Stein", "Other"]
-
 def get_zeroed_dictionary(row):
     dictionary = dict([(name, 0) for name in candidate_names])
     for field in fields_to_match:
@@ -37,12 +31,26 @@ def translate_infile_to_outfile(reader, writer):
             current_dict[candidate] = votes
     writer.writerow(current_dict)
 
+def parse_and_translate_files(infile_path, outfile_path, candidate_names):
+    with open(infile_path) as infile:
+        reader = csv.DictReader(infile)
+        with open(outfile_path, "w") as outfile:
+            writer = csv.DictWriter(outfile, fieldnames = fields_to_match + candidate_names)
+            translate_infile_to_outfile(reader, writer)
 
-with open(infile_path) as infile:
-    reader = csv.DictReader(infile)
-    with open(outfile_path, "w") as outfile:
-        writer = csv.DictWriter(outfile, fieldnames = fields_to_match + candidate_names)
-        translate_infile_to_outfile(reader, writer)
+            
+fields_to_match = ["abbr_state","county","fips"]
 
+infile_path = "./county_election_results_2016.csv"
+outfile_path = "./county_election_results_2016_row_per_county.csv"
+candidate_names = ["Donald Trump", "Hillary Clinton", "Gary Johnson", "Jill Stein", "Other"]
+
+parse_and_translate_files(infile_path, outfile_path, candidate_names)
+
+infile_path = infile_path.replace(".csv", "_senate.csv")
+outfile_path = outfile_path.replace(".csv", "_senate.csv")
+candidate_names = ["democrat", "republican", "other"]
+
+parse_and_translate_files(infile_path, outfile_path, candidate_names)
 
 
